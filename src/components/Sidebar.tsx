018b8e3ea7bad2cf058bb07bf71f189f324b2a04
meena9697 <meenasundaram9697@gmail.com>
2022-06-28 22:35:35 +0200

    changed the options in side bar and added a notification pop up

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,14 +5,16 @@ import Drawer from "@mui/material/Drawer";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
 import HomeIcon from "@mui/icons-material/Home";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ArticleIcon from '@mui/icons-material/Article';
-import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 const drawerWidth = 240;
+export interface State extends SnackbarOrigin {
+  open: boolean;
+}
 
 interface Props {
   /**
@@ -23,25 +25,79 @@ interface Props {
 }
 
 export default function ResponsiveDrawer(props: Props) {
+  const [state, setState] = React.useState<State>({
+    open: false,
+    vertical: "top",
+    horizontal: "left",
+  });
+  const { vertical, horizontal, open } = state;
+
+  const handleClick = (newState: SnackbarOrigin) => () => {
+    setState({ open: true, ...newState });
+  };
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-  const itemList = [{icon:<><HomeIcon sx={{marginRight: '1rem'}}/></>, name: 'Maintenance'}, {icon:<><BusinessCenterIcon sx={{marginRight: '1rem'}}/></>, name: 'New rental'}, {icon:<><ArticleIcon sx={{marginRight: '1rem'}}/></>, name: 'Core data'}, {icon:<><FavoriteBorderIcon sx={{marginRight: '1rem'}}/></>, name: 'Favourites'}, {icon:<><CardGiftcardIcon sx={{marginRight: '1rem'}}/></>, name: 'Premium upgrade'}];
-    const drawer = (
+  const itemList = [
+    {
+      icon: (
+        <>
+          <HomeIcon sx={{ marginRight: "1rem" }} />
+        </>
+      ),
+      name: "Maintenance",
+    },
+    {
+      icon: (
+        <>
+          <BusinessCenterIcon sx={{ marginRight: "1rem" }} />
+        </>
+      ),
+      name: "New rental",
+    },
+    {
+      icon: (
+        <>
+          <ChatBubbleOutlineIcon sx={{ marginRight: "1rem" }} />
+        </>
+      ),
+      name: "Chat",
+    },
+  ];
+  const drawer = (
     <div>
-      <Toolbar />
       <Divider />
       <ListItemButton>
-        <ListItemText>EXPLORE MORE</ListItemText>
+        <ListItemText> EXPLORE MORE</ListItemText>
       </ListItemButton>
       <Divider />
-      {itemList.map(item => (<ListItem><ListItemButton>{item.icon}{item.name}</ListItemButton></ListItem>)
-        )}
-
-
+      {itemList.map((item) => (
+        <ListItem>
+          <ListItemButton>
+            {item.icon}
+            {item.name}
+          </ListItemButton>
+        </ListItem>
+      ))}
+      <ListItemButton
+        onClick={handleClick({
+          vertical: "top",
+          horizontal: "center",
+        })}
+      >
+        <>
+          <NotificationsNoneIcon sx={{ marginLeft: "1rem" }} />
+        </>
+        <ListItemText sx={{ marginLeft: "1rem" }}>Notification</ListItemText>
+      </ListItemButton>
       <Divider />
     </div>
   );
@@ -62,6 +118,14 @@ export default function ResponsiveDrawer(props: Props) {
       >
         {drawer}
       </Drawer>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={4000}
+        key={vertical + horizontal}
+        message=" Rent Due on DD/MM/YYYY. Don't forget to pay.."
+      />
     </Box>
   );
 }
