181e0b61d6fc3ff0977eb836da79baa04a3dac6d
meena9697 <meenasundaram9697@gmail.com>
2022-06-28 18:30:08 +0200

    made some adjustment in the sidebar since it was affecting navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -55,6 +55,7 @@ export default function ResponsiveDrawer(props: Props) {
           "& .MuiDrawer-paper": {
             boxSizing: "border-box",
             width: drawerWidth,
+            marginTop: "5rem",
           },
         }}
         open
